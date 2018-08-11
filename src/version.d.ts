fae859c571df25f6149bb252db6c70b7976b723b
zi0xtk <ziwuafter@outlook.com>
2018-08-11 00:08:21 +0000

    ­

--- src/version.d.ts
+++ src/version.d.ts
@@ -1,2 +1,2 @@
-export declare const AUI_VERSION = "1.7.0-beta.4ih8i";
-export declare const AUI_LAST_UPDATE = "2018-08-10T23:36:05.786Z";
+export declare const AUI_VERSION = "1.7.0-beta.4iic8";
+export declare const AUI_LAST_UPDATE = "2018-08-10T23:59:56.290Z";
