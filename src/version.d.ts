4e28ac018939c5868489e4c0c34ffbd37bf4e378
zi0xtk <ziwuafter@outlook.com>
2018-08-29 08:00:48 +0000

    ­

--- src/version.d.ts
+++ src/version.d.ts
@@ -1,2 +1,2 @@
-export declare const AUI_VERSION = "1.7.0-beta.5gch8";
-export declare const AUI_LAST_UPDATE = "2018-08-29T06:31:47.065Z";
+export declare const AUI_VERSION = "1.7.0-beta.5gg5p";
+export declare const AUI_LAST_UPDATE = "2018-08-29T07:51:16.254Z";
