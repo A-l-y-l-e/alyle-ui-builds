4da3a6ea1ef5e44ed467c3af3a0c98dfed299d7e
zi0xtk <ziwuafter@outlook.com>
2018-09-19 08:35:59 +0000

    ­

--- esm2015/responsive/index.js
+++ esm2015/responsive/index.js
@@ -2,6 +2,6 @@
  * @fileoverview added by tsickle
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
-export { MediaDirective, ResponsiveModule, LY_MEDIA_QUERIES, Breakpoints } from './public_api';
+export { MediaDirective, ResponsiveModule, Breakpoints } from './public_api';
 
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290Ijoibmc6Ly9AYWx5bGUvdWkvcmVzcG9uc2l2ZS8iLCJzb3VyY2VzIjpbImluZGV4LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7QUFBQSxnRkFBYyxjQUFjLENBQUMiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgKiBmcm9tICcuL3B1YmxpY19hcGknO1xuIl19
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290Ijoibmc6Ly9AYWx5bGUvdWkvcmVzcG9uc2l2ZS8iLCJzb3VyY2VzIjpbImluZGV4LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7QUFBQSw4REFBYyxjQUFjLENBQUMiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgKiBmcm9tICcuL3B1YmxpY19hcGknO1xuIl19
--- responsive/tokens.d.ts
+++
@@ -1,6 +0,0 @@
-import { InjectionToken } from '@angular/core';
-/** @deprecated */
-export declare const LY_MEDIA_QUERIES: InjectionToken<LyMediaQueries>;
-export interface LyMediaQueries {
-    [key: string]: string;
-}
