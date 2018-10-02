45ac0896b999ec83de015038295b7d5df5c6d306
alyleui <alyleui@gmail.com>
2018-10-02 20:34:02 +0000

    ­

--- src/version.d.ts
+++ src/version.d.ts
@@ -1,2 +1,2 @@
-export declare const AUI_VERSION = "1.7.0-nightly.20181002-jms5xhe2";
-export declare const AUI_LAST_UPDATE = "2018-10-02T20:15:42.408Z";
+export declare const AUI_VERSION = "1.7.0-nightly.20181002-jms6ha9b";
+export declare const AUI_LAST_UPDATE = "2018-10-02T20:31:06.286Z";
