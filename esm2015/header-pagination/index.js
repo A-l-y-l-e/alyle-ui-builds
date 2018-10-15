fc72741892236bad0f1d1cb134c4861c06213724
alyleui <alyleui@gmail.com>
2018-10-15 08:30:10 +0000

    ­

--- esm2015/header-pagination/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * @fileoverview added by tsickle
- * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
- */
-export { LyHeaderPaginationModule, LyHeaderPagination } from './public_api';
-
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290Ijoibmc6Ly9AYWx5bGUvdWkvaGVhZGVyLXBhZ2luYXRpb24vIiwic291cmNlcyI6WyJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7O0FBQUEsNkRBQWMsY0FBYyxDQUFDIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0ICogZnJvbSAnLi9wdWJsaWNfYXBpJztcbiJdfQ==
--- src/version.d.ts
+++ src/version.d.ts
@@ -1,2 +1,2 @@
-export declare const AUI_VERSION = "1.7.2-nightly.20181014-jn8luapr";
-export declare const AUI_LAST_UPDATE = "2018-10-14T08:25:26.460Z";
+export declare const AUI_VERSION = "1.7.2-nightly.20181015-jna1dkjo";
+export declare const AUI_LAST_UPDATE = "2018-10-15T08:28:06.082Z";
