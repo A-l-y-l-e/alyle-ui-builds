01b4905701a4e0e12d27416f9cb49a21488b77b0
Enmanuel <Enlcxx.es@gmail.com>
2018-10-24 08:26:12 +0000

    ­

--- src/version.d.ts
+++ src/version.d.ts
@@ -1,2 +1,2 @@
-export declare const AUI_VERSION = "1.7.4-nightly.20181023-jnlgs3sk";
-export declare const AUI_LAST_UPDATE = "2018-10-23T08:24:46.386Z";
+export declare const AUI_VERSION = "1.7.4-nightly.20181024-jnmw6zu9";
+export declare const AUI_LAST_UPDATE = "2018-10-24T08:24:01.520Z";
