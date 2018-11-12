8ec6815303983a13f50627aba00d6ccd539932e8
alyleui <alyleui@gmail.com>
2018-11-12 08:10:03 +0000

    ­

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -3,8 +3,8 @@
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-export const AUI_VERSION = '1.7.10';
+export const AUI_VERSION = '1.7.11-nightly.20181112-joe0zbhx';
 /** @type {?} */
-export const AUI_LAST_UPDATE = '2018-11-12T07:55:03.166Z';
+export const AUI_LAST_UPDATE = '2018-11-12T08:07:48.211Z';
 
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidmVyc2lvbi5qcyIsInNvdXJjZVJvb3QiOiJuZzovL0BhbHlsZS91aS8iLCJzb3VyY2VzIjpbInNyYy92ZXJzaW9uLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsYUFBYSxXQUFXLEdBQUcsUUFBUSxDQUFDOztBQUNwQyxhQUFhLGVBQWUsR0FBRywwQkFBMEIsQ0FBQyIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBjb25zdCBBVUlfVkVSU0lPTiA9ICcxLjcuMTAnO1xuZXhwb3J0IGNvbnN0IEFVSV9MQVNUX1VQREFURSA9ICcyMDE4LTExLTEyVDA3OjU1OjAzLjE2NlonO1xuIl19
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidmVyc2lvbi5qcyIsInNvdXJjZVJvb3QiOiJuZzovL0BhbHlsZS91aS8iLCJzb3VyY2VzIjpbInNyYy92ZXJzaW9uLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsYUFBYSxXQUFXLEdBQUcsa0NBQWtDLENBQUM7O0FBQzlELGFBQWEsZUFBZSxHQUFHLDBCQUEwQixDQUFDIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGNvbnN0IEFVSV9WRVJTSU9OID0gJzEuNy4xMS1uaWdodGx5LjIwMTgxMTEyLWpvZTB6Ymh4JztcbmV4cG9ydCBjb25zdCBBVUlfTEFTVF9VUERBVEUgPSAnMjAxOC0xMS0xMlQwODowNzo0OC4yMTFaJztcbiJdfQ==
--- src/version.d.ts
+++ src/version.d.ts
@@ -1,2 +1,2 @@
-export declare const AUI_VERSION = "1.7.10";
-export declare const AUI_LAST_UPDATE = "2018-11-12T07:55:03.166Z";
+export declare const AUI_VERSION = "1.7.11-nightly.20181112-joe0zbhx";
+export declare const AUI_LAST_UPDATE = "2018-11-12T08:07:48.211Z";
