afd47b0b0869e48b19c5175d75fcfa2744a30052
Enlcxx <8032887+Enlcxx@users.noreply.github.com>
2018-12-18 08:25:55 +0000

    ­

--- src/version.d.ts
+++ src/version.d.ts
@@ -1,2 +1,2 @@
-export declare const AUI_VERSION = "1.9.8";
-export declare const AUI_LAST_UPDATE = "2018-12-17T23:06:35.094Z";
+export declare const AUI_VERSION = "1.9.9-nightly.20181218-jpthezzx";
+export declare const AUI_LAST_UPDATE = "2018-12-18T08:24:08.636Z";
