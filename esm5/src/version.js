0e8cfa423ec061780d51b5e3d6839f0aa52fceb9
Enlcxx <8032887+Enlcxx@users.noreply.github.com>
2018-12-23 08:24:41 +0000

    ­

--- esm5/src/version.js
+++ esm5/src/version.js
@@ -3,7 +3,7 @@
  * @suppress {checkTypes,extraRequire,missingReturn,uselessCode} checked by tsc
  */
 /** @type {?} */
-export var AUI_VERSION = '1.9.11-nightly.20181222-jpz74nq3';
+export var AUI_VERSION = '1.9.11-nightly.20181223-jq0mkjl5';
 /** @type {?} */
-export var AUI_LAST_UPDATE = '2018-12-22T08:22:47.065Z';
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidmVyc2lvbi5qcyIsInNvdXJjZVJvb3QiOiJuZzovL0BhbHlsZS91aS8iLCJzb3VyY2VzIjpbInNyYy92ZXJzaW9uLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsTUFBTSxLQUFPLFdBQVcsR0FBRyxrQ0FBa0M7O0FBQzdELE1BQU0sS0FBTyxlQUFlLEdBQUcsMEJBQTBCIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGNvbnN0IEFVSV9WRVJTSU9OID0gJzEuOS4xMS1uaWdodGx5LjIwMTgxMjIyLWpwejc0bnEzJztcbmV4cG9ydCBjb25zdCBBVUlfTEFTVF9VUERBVEUgPSAnMjAxOC0xMi0yMlQwODoyMjo0Ny4wNjVaJztcbiJdfQ==
+export var AUI_LAST_UPDATE = '2018-12-23T08:22:48.615Z';
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidmVyc2lvbi5qcyIsInNvdXJjZVJvb3QiOiJuZzovL0BhbHlsZS91aS8iLCJzb3VyY2VzIjpbInNyYy92ZXJzaW9uLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsTUFBTSxLQUFPLFdBQVcsR0FBRyxrQ0FBa0M7O0FBQzdELE1BQU0sS0FBTyxlQUFlLEdBQUcsMEJBQTBCIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGNvbnN0IEFVSV9WRVJTSU9OID0gJzEuOS4xMS1uaWdodGx5LjIwMTgxMjIzLWpxMG1ramw1JztcbmV4cG9ydCBjb25zdCBBVUlfTEFTVF9VUERBVEUgPSAnMjAxOC0xMi0yM1QwODoyMjo0OC42MTVaJztcbiJdfQ==
--- src/version.d.ts
+++ src/version.d.ts
@@ -1,2 +1,2 @@
-export declare const AUI_VERSION = "1.9.11-nightly.20181222-jpz74nq3";
-export declare const AUI_LAST_UPDATE = "2018-12-22T08:22:47.065Z";
+export declare const AUI_VERSION = "1.9.11-nightly.20181223-jq0mkjl5";
+export declare const AUI_LAST_UPDATE = "2018-12-23T08:22:48.615Z";
