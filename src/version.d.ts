db281221d60daa8714cbb1e489e0c2cc434752c7
Enlcxx <8032887+Enlcxx@users.noreply.github.com>
2018-12-30 08:25:02 +0000

    ­

--- src/version.d.ts
+++ src/version.d.ts
@@ -1,2 +1,2 @@
-export declare const AUI_VERSION = "2.0.0";
-export declare const AUI_LAST_UPDATE = "2018-12-30T05:08:08.366Z";
+export declare const AUI_VERSION = "2.0.1-nightly.20181230-jqamnxnw";
+export declare const AUI_LAST_UPDATE = "2018-12-30T08:23:08.586Z";
