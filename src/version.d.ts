0c6ca775aeda0851abd458adf575cbd2119f086e
Enmanuel <8032887+Enlcxx@users.noreply.github.com>
2019-01-03 08:24:08 +0000

    ­

--- src/version.d.ts
+++ src/version.d.ts
@@ -1,2 +1,2 @@
-export declare const AUI_VERSION = "2.0.2-nightly.20190102-jqewz700";
-export declare const AUI_LAST_UPDATE = "2019-01-02T08:22:54.767Z";
+export declare const AUI_VERSION = "2.0.2-nightly.20190103-jqgce8wt";
+export declare const AUI_LAST_UPDATE = "2019-01-03T08:22:17.500Z";
