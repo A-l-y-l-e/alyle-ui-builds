4718646482a0bb20148e40fb7e591dbc39d5a10c
Enlcxx <Enlcxx.es@gmail.com>
2020-02-04 21:26:58 +0000

    fix some bugs
    c39ea26c42e69968b512448d9d32619c7e34b47a

--- bundles/alyle-ui.umd.js
+++ bundles/alyle-ui.umd.js
@@ -3592,8 +3592,8 @@
         return LyFocusState;
     }());
 
-    var AUI_VERSION = '2.9.8-nightly.2001250041';
-    var AUI_LAST_UPDATE = '2020-01-25T00:41:28.617Z';
+    var AUI_VERSION = '2.9.8-preview.6cee32';
+    var AUI_LAST_UPDATE = '2020-02-04T21:19:54.156Z';
 
     var LY_HAMMER_OPTIONS = new core.InjectionToken('LY_HAMMER_OPTIONS');
     var HAMMER_GESTURES_EVENTS = [
--- src/version.d.ts
+++ src/version.d.ts
@@ -1,2 +1,2 @@
-export declare const AUI_VERSION = "2.9.8-nightly.2001250041";
-export declare const AUI_LAST_UPDATE = "2020-01-25T00:41:28.617Z";
+export declare const AUI_VERSION = "2.9.8-preview.6cee32";
+export declare const AUI_LAST_UPDATE = "2020-02-04T21:19:54.156Z";
